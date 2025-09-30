715722ecdb3d72bb841ae4c96f1e3cf8f2f17d81
Atunde <130572243+Atunde-SS@users.noreply.github.com>
2025-09-30 15:07:28 +0100

    Delete zama-ai-tutor directory

--- zama-ai-tutor/components/icons/BrowserIcon.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const BrowserIcon: React.FC<{ className?: string }> = ({ className = "h-10 w-10" }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-        <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9" />
-    </svg>
-);
